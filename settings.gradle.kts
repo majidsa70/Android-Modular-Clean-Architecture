aae119879fd96ccfd35429da3629424c9b76055c
Majid <majid.sa70@gmail.com>
2024-05-02 20:05:24 +0330

    clean architecture feature modular

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Modular Clean Architecture"
+include(":app")
+include(":feature-one:data")
+include(":feature-one:ui")
+include(":feature-one:domain")
+include(":feature-one:di")
